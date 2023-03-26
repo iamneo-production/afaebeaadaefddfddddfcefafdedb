4acba3fe6c4cd16747a534a16cdf3acd732498b5
root <root@afaebeaadaefddfddddfcefafdedb-0.afaebeaadaefddfddddfcefafdedb.che.svc.cluster.local>
2023-03-26 17:10:05 +0000

    Backup Branch before deleting

---
+++ jspservlet-app-binarytravels/src/main/java/services/Booking.java
@@ -0,0 +1,10 @@
+package services;
+
+public class Booking{
+//	Enter code here...
+   public double booking(int noOfPersons,int rates){
+       double totalRate=noOfPersons*rates;
+       return totalRate;
+   }
+}
+
---
+++ jspservlet-app-binarytravels/src/main/java/services/FareCalculator.java
@@ -0,0 +1,42 @@
+package services;
+
+import java.time.temporal.ChronoUnit;
+
+import model.Bus;
+import model.Flight;
+import model.Hotel;
+import model.Train;
+
+public class FareCalculator extends Booking{
+//Enter code here...
+    public double book(Hotel hotel){
+        double totalFare=0;
+        if(hotel.getFromdate().isBefore(hotel.getTodate())){
+            totalFare=booking(hotel.getNoOfPersons(),hotel.getRates());
+        }
+        return totalFare;
+    }
+    public double book(Flight flight){
+        double totalFare=booking(flight.getNoOfPersons(),flight.getRates());
+        if(flight.getTriptype().equalsIgnoreCase("one-way")){
+            return totalFare;
+        }
+        else{
+            int diff=(flight.getFrom()).compareTo(flight.getTo());
+            if(diff<0){
+                return 2*totalFare;
+            }
+            else{
+                return 0;
+            }
+        }
+    }
+    public double book(Train train){
+        double totalFare=booking(train.getNoOfPersons(), train.getRates());
+        return totalFare;
+    }
+    public double book(Bus bus){
+        double totalFare=booking(bus.getNoOfPersons(),bus.getRates());
+        return totalFare;
+    }
+}
